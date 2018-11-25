59222babf69a29abe6b87ca285df08e58fa916d3
ZygoMatic74 <37933761+ZygoMatic74@users.noreply.github.com>
2018-11-25 15:56:39 +0100

    Génération automatique du formulaire
    Mise en place de la fonction qui génère complètement la vue en fonction du JSON

--- JSON/myJsonInterface.cs
+++ JSON/myJsonInterface.cs
@@ -18,58 +18,46 @@ namespace AppXamarin
 {
   class myJsonInterface
   {
-    myServicesListe myServices = new myServicesListe();
+        myServicesListe myServices = new myServicesListe();
+        String serviceSelected = "null";
 
-    public myJsonInterface(string nameJsonFile, AssetManager assets)
-    {
-      string content;
-      using (StreamReader sr = new StreamReader(assets.Open(nameJsonFile)))
-      {
-        content = sr.ReadToEnd();
-        myServices = JsonConvert.DeserializeObject<myServicesListe>(content);
-      }
-    }
+        public myJsonInterface(string nameJsonFile, AssetManager assets)
+        {
+          string content;
+          using (StreamReader sr = new StreamReader(assets.Open(nameJsonFile)))
+          {
+            content = sr.ReadToEnd();
+            myServices = JsonConvert.DeserializeObject<myServicesListe>(content);
+          }
+        }
 
-    public myServicesListe getServices()
+        public myServicesListe getServices()
         {
             return myServices;
         }
 
-    public void generateJsonForm(LinearLayout myView, Context ctxt)
+
+        // ***********************************************************************************************
+        // *****************   GENERATION DU FORMULAIRE POUR LE SERVICE SELECTIONNE        ***************
+        // ***********************************************************************************************
+
+        public void generateJsonForm(LinearLayout myView, Context ctxt)
         {
             ISharedPreferences prefs = PreferenceManager.GetDefaultSharedPreferences(ctxt);
             String myServiceString = prefs.GetString("currentService", "null");
 
-            myView.RemoveAllViewsInLayout();
-
             if(myServiceString != "null")
             {
-                if (myServiceString == myServices.services[0].title)
+                if (myServiceString != serviceSelected)
                 {
-                    // Définition du composant Text. 
-                    TextView myTextView = new TextView(ctxt);
-                    myTextView.Text = myServices.services[0].title;
-                    myView.AddView(myTextView);
+                    createFormForService(myServiceString, myView, ctxt);
+                    serviceSelected = myServiceString;
                 }
-
-                if (myServiceString == myServices.services[1].title)
-                {
-                    TextView myTextView2 = new TextView(ctxt);
-                    myTextView2.Text = myServices.services[1].title;
-                    myView.AddView(myTextView2);
-                }
-
-                RadioGroup r = new RadioGroup(ctxt);
-                String test = "Test";
-
-                RadioButton r1 = new RadioButton(ctxt);
-                r1.Text = test;
-
-                r.AddView(r1);
-                myView.AddView(r);
             }
             else
             {
+                myView.RemoveAllViewsInLayout();
+
                 TextView myTextView = new TextView(ctxt);
                 myTextView.Text = "Aucun service sélectionné !";
                 myView.AddView(myTextView);
@@ -77,7 +65,120 @@ namespace AppXamarin
 
         }
 
-    public void generateJsonServiceList(LinearLayout myList, Context ctxt)
+        private void createFormForService(String myService, LinearLayout myView, Context ctxt)
+        {
+            myView.RemoveAllViewsInLayout();
+
+            int indiceService = getIndiceForService(myService);
+            myElement[] tabElements = myServices.services[indiceService].elements;
+
+            for(int i = 0; i < tabElements.Length; i++)
+            {
+                switch (tabElements[i].type)
+                {
+                    case "image" :
+                        addImageToView(tabElements[i].value[0], myView, ctxt);
+                        break;
+                    case "edit":
+                        addEditTextToView(tabElements[i].value[0], myView, ctxt);
+                        break;
+                    case "radioGroup":
+                        addRadioGroupToView(tabElements[i].value, myView, ctxt);
+                        break;
+                    case "label":
+                        addLabelToView(tabElements[i].value[0], myView, ctxt);
+                        break;
+                    case "switch":
+                        addSwitchToView(tabElements[i].value[0], myView, ctxt);
+                        break;
+                    case "button":
+                        addButtonToView(tabElements[i].value[0], myView, ctxt);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private int getIndiceForService(String myService)
+        {
+            for(int i = 0; i < myServices.services.Length; i++)
+            {
+                if(myServices.services[i].title == myService) { return i; }
+            }
+            return -1;
+        }
+
+        private void addImageToView(String myUrl, LinearLayout myView, Context ctxt)
+        {
+            ImageView myImage = new ImageView(ctxt);
+            var uri = GetImageBitmapFromUrl(myUrl);
+            myImage.SetImageBitmap(uri);
+            myImage.SetScaleType(ImageView.ScaleType.FitCenter);
+            myView.AddView(myImage);
+        }
+
+        private void addEditTextToView(String myHint, LinearLayout myView, Context ctxt)
+        {
+            EditText myEdit = new EditText(ctxt);
+            myEdit.Hint = myHint;
+            myView.AddView(myEdit);
+        }
+
+        private void addRadioGroupToView(String[] myValues, LinearLayout myView, Context ctxt)
+        {
+            RadioGroup myRadioGroup = new RadioGroup(ctxt);
+
+            for(int i = 0; i < myValues.Length; i++)
+            {
+                RadioButton myRadioButton = new RadioButton(ctxt);
+
+                String myValue = myValues[i];
+                myRadioButton.Text = myValue;
+
+                myRadioGroup.AddView(myRadioButton);
+            }
+
+            myView.AddView(myRadioGroup);
+        }
+
+        private void addLabelToView(String myLabel, LinearLayout myView, Context ctxt)
+        {
+            TextView myText = new TextView(ctxt);
+            myText.Text = myLabel;
+
+            myView.AddView(myText);
+        }
+
+        private void addSwitchToView(String myBool, LinearLayout myView, Context ctxt)
+        {
+            Switch mySwitch = new Switch(ctxt);
+
+            if (myBool == "true")
+            {
+                mySwitch.Checked = true;
+            }
+            else
+            {
+                mySwitch.Checked = false;
+            }
+
+            myView.AddView(mySwitch);
+        }
+
+        private void addButtonToView(String myText, LinearLayout myView, Context ctxt)
+        {
+            Button myButton = new Button(ctxt);
+            myButton.Text = myText;
+
+            myView.AddView(myButton);
+        }
+
+        // ***********************************************************************************************
+        // *********************   GENERATION DE LA LISTE DES SERVICES         ***************************
+        // ***********************************************************************************************
+
+        public void generateJsonServiceList(LinearLayout myList, Context ctxt)
         {
             for(int i = 0; i < myServices.services.Length; i++)
             {
@@ -99,8 +200,6 @@ namespace AppXamarin
                 myList.AddView(myImage);
             }
         }
-
-
 
         // ***** Fonction permettant de récupérer une image en focntion de son URL et la converti en Bitmap
         // ***** Trouvée sur un forum
